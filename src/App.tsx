997db27101c331bb4797208dd8911704ffa6fe7e
guilhermescr <guilhermechaves2005@hotmail.com>
2023-08-03 18:27:07 -0300

    started the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-3xl text-slate-700 mt-8">
+        Hello, world (once again)!
+      </h1>
+    </>
+  );
+}
+
+export default App;
